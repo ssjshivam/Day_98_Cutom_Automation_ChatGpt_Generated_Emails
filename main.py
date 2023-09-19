e688ea8fa73d0706ae36c765c969c72c246b99e8
Surya Pratap Singh <ssjshivam2777@gmail.com>
2023-09-19 11:36:42 +0530

    updated the code so image will be displayed as attachment

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
+import requests
 import openai
-import os
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
 import random
+import os
 
 # Credentials
-MY_EMAIL = "ENTER SENDER'S ADDRESS"
-OUT_EMAIL = "ENTER RECEIVER'S ADDRESS"
-PASSWORD = "ENTER THE WEBAPP PASSWORD"
+MY_EMAIL = "Enter sender email"
+OUT_EMAIL = "enter receiver's email"
+PASSWORD = "enter your password"
 
 motivational_topics = [
     "Goal setting and achievement",
@@ -59,7 +61,7 @@ answer = response.choices[0]["text"]
 
 # print("Answer:", answer)
 
-# Ai Generated Image
+# Ai Generated Image --------------------------------------------------------------------------------------------------
 
 response_image = openai.Image.create(
     prompt=f"Beautiful Abstract painting of {random.choice(couples_images)}",
@@ -67,6 +69,23 @@ response_image = openai.Image.create(
     size="512x512",
 )
 image_url = response_image["data"][0]["url"]
+print(image_url)
+
+url = image_url
+filename = "image.jpg"
+
+image_response = requests.get(url)
+if image_response.status_code == 200:
+    with open(filename, 'wb') as file:
+        file.write(image_response.content)
+    file.close()
+    if os.path.exists(filename):
+        print("Image saved successfully.")
+    else:
+        print("Failed to save image.")
+
+with open(filename, 'rb') as f:
+    img_data = f.read()
 
 # SENT THE EMAIL--------------------------------------------------------------------------------------------------------
 
@@ -91,22 +110,19 @@ with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
         <h1 style="text-align:center; font-family: 'Caveat', cursive;display: block;
         margin-left: auto;
         margin-right: auto;">{answer}</h1>
-        <img style="display: block;
-        margin-left: auto;
-        margin-right: auto;" src={image_url}>
       </body>
     </html>
     """
 
     # Record the MIME types of both parts - text/plain and text/html. (ONLY JUST HTML FOR NOW)
-    # part1 = MIMEText(text, 'plain')
-    part2 = MIMEText(html, 'html')
+    part1 = MIMEText(html, 'html')
 
     # Attach parts into message container.
     # According to RFC 2046, the last part of a multipart message, in this case
     # the HTML message, is best and preferred.
-    # msg.attach(part1)
-    msg.attach(part2)
+    msg.attach(part1)
+    image = MIMEImage(img_data, name=os.path.basename(filename))
+    msg.attach(image)
 
     connection.starttls()
     connection.login(user=MY_EMAIL, password=PASSWORD)
